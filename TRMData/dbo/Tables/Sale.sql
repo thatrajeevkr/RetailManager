1f5facbf222f98f5d55ab4dc5fcdf2db19d724a0
Rajeev Ravi <thatrajeevkr@gmail.com>
2021-02-06 19:49:47 +0100

    Register DB Tables
    The tables for the basic register system

---
+++ TRMData/dbo/Tables/Sale.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Sale]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [CasherId] NVARCHAR(128) NOT NULL, 
+    [SaleDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+    [SubTotal] MONEY NOT NULL, 
+    [Tax] MONEY NOT NULL, 
+    [Total] MONEY NOT NULL
+)
---
+++ TRMData/dbo/Tables/SaleDetail.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[SaleDetail]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [SaleId] INT NOT NULL, 
+    [ProductId] INT NOT NULL, 
+    [PurchasePrice] MONEY NOT NULL, 
+    [Quantity] INT NOT NULL DEFAULT 1,
+    [Tax] MONEY NOT NULL DEFAULT 0
+)
