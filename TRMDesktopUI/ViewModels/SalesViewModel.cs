1003f063721a84ed9d8f8bdaa0fa9257be452764
Rajeev Ravi <thatrajeevkr@gmail.com>
2021-02-18 14:15:08 +0100

    Design WPF Sales Page

---
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -0,0 +1,126 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRMDesktopUI.ViewModels
+{
+    public class SalesViewModel : Screen
+    {
+        private BindingList<String> _products;
+
+        public BindingList<String> Products
+        {
+            get { return _products; }
+            set { 
+                _products = value;
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
+
+        private BindingList<string> _cart;
+
+        public BindingList<string> Cart
+        {
+            get { return _cart; }
+            set { 
+                _cart = value;
+                NotifyOfPropertyChange(() => Cart);
+            }
+        }
+
+
+        private string _itemQuantity;
+
+        public string ItemQuantity
+        {
+            get { return _itemQuantity; }
+            set {
+                _itemQuantity = value;
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
+
+
+        public string SubTotal
+        {
+            get 
+            { 
+                //TODO - Replace with Calculation
+                return "$0.00"; 
+            }
+        }
+
+        public string TAX
+        {
+            get
+            {
+                //TODO - Replace with Calculation
+                return "$0.00";
+            }
+        }
+
+        public string Total
+        {
+            get
+            {
+                //TODO - Replace with Calculation
+                return "$0.00";
+            }
+        }
+
+
+        public bool CanAddToCart
+        {
+            get
+            {
+                bool output = false;
+
+                //Make sure something is selected
+                //Make sure there is an Item quantity
+                return output;
+            }
+        }
+
+        public void AddToCart()
+        {
+
+        }
+
+        public bool CanRemoveFromCart
+        {
+            get
+            {
+                bool output = false;
+
+                //Make sure something is selected
+                return output;
+            }
+        }
+
+        public void RemoveFromCart()
+        {
+
+        }
+
+        public bool CanCheckOut
+        {
+            get
+            {
+                bool output = false;
+
+                //Make sure something is in the cart
+                return output;
+            }
+        }
+
+        public void CheckOut()
+        {
+
+        }
+
+    }
+}
