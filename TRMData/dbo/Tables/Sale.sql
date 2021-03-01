7353e876b4fd0fd04b9ceb869a3acaed6f777cab
Rajeev Ravi <thatrajeevkr@gmail.com>
2021-03-01 16:07:25 +0100

    Add Foreign Key Relationships

--- TRMData/dbo/Tables/Sale.sql
+++ TRMData/dbo/Tables/Sale.sql
@@ -5,5 +5,6 @@
     [SaleDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
     [SubTotal] MONEY NOT NULL, 
     [Tax] MONEY NOT NULL, 
-    [Total] MONEY NOT NULL
+    [Total] MONEY NOT NULL, 
+    CONSTRAINT [FK_Sale_ToUser] FOREIGN KEY (CasherId) REFERENCES [User](Id)
 )
