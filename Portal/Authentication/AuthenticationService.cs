c1c416d495cd7a416c765f27fcd748c491b38d5f
Rajeev Ravi <thatrajeevkr@gmail.com>
2021-03-25 09:19:15 +0100

    Adding Authorization Class for Blazor

---
+++ Portal/Authentication/AuthenticationService.cs
@@ -0,0 +1,63 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using Portal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Portal.Authentication
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly HttpClient _client;
+        private readonly ILocalStorageService _localStorage;
+        private readonly AuthenticationStateProvider _authStateProvider;
+
+        public AuthenticationService(ILocalStorageService localStorage, HttpClient client, AuthenticationStateProvider authStateProvider)
+        {
+            _localStorage = localStorage;
+            _client = client;
+            _authStateProvider = authStateProvider;
+        }
+
+        public async Task<AuthenticatedUserModel> Login(AuthenticationUserModel userForAuthentication)
+        {
+            var data = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("grant_type", "password"),
+                new KeyValuePair<string, string>("username", userForAuthentication.Email),
+                new KeyValuePair<string, string>("password", userForAuthentication.Password)
+            });
+
+            var authResult = await _client.PostAsync("https://localhost:5001/token", data);
+            var authContent = await authResult.Content.ReadAsStringAsync();
+
+            if (authResult.IsSuccessStatusCode == false)
+            {
+                return null;
+            }
+
+            var result = JsonSerializer.Deserialize<AuthenticatedUserModel>(
+                authContent,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            await _localStorage.SetItemAsync("authToken", result.Access_Token);
+
+            ((AuthStateProvider)_authStateProvider).NotifyUserAuthentication(result.Access_Token);
+
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", result.Access_Token);
+
+            return result;
+        }
+
+        public async Task LogOut()
+        {
+            await _localStorage.RemoveItemAsync("authToken");
+            ((AuthStateProvider)_authStateProvider).NotifyUserLogOut();
+            _client.DefaultRequestHeaders.Authorization = null;
+        }
+    }
+}
--- Portal/Program.cs
+++ Portal/Program.cs
@@ -1,7 +1,10 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Portal.Authentication;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -16,6 +19,11 @@ namespace Portal
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+            builder.Services.AddBlazoredLocalStorage();
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
