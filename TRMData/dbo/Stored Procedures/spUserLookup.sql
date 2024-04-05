dd4838204f7bee46bceda559ad9071bb88c5376f
Rajeev Ravi <thatrajeevkr@gmail.com>
2024-04-05 16:50:35 +0800

    Add spUserLookup.sql

---
+++ TRMData/dbo/Stored Procedures/spUserLookup.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spUserLookup]
+	@Id nvarchar(128)
+AS
+begin
+	set nocount on;
+
+	select Id, FirstName, LastName, EmailAddress, CreateDate
+	from [dbo].[User]
+	where Id = @Id;
+end
