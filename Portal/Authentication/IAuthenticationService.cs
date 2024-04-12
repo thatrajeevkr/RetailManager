fe5c9ca8af386b326ba4025a98f4fd920ad4e525
Rajeev Ravi <thatrajeevkr@gmail.com>
2024-04-12 15:40:57 +0800

    Add IAuthenticationService.cs

---
+++ Portal/Authentication/IAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using Portal.Models;
+using System.Threading.Tasks;
+
+namespace Portal.Authentication
+{
+    public interface IAuthenticationService
+    {
+        Task<AuthenticatedUserModel> Login(AuthenticationUserModel userForAuthentication);
+        Task LogOut();
+    }
+}
