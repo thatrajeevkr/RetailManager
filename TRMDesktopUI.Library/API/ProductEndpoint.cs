29f6a33a3c526c82f6674521bd9e0bbfbd61d932
Rajeev Ravi <thatrajeevkr@gmail.com>
2024-04-20 06:08:08 +0800

    Add ProductEndpoint.cs

---
+++ TRMDesktopUI.Library/API/ProductEndpoint.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TRMDesktopUI.Library.Models;
+
+namespace TRMDesktopUI.Library.API
+{
+    public class ProductEndpoint : IProductEndpoint
+    {
+        private IAPIHelper _apiHelper;
+        public ProductEndpoint(IAPIHelper aPIHelper)
+        {
+            _apiHelper = aPIHelper;
+        }
+
+        public async Task<List<ProductModel>> GetAll()
+        {
+            using (HttpResponseMessage response = await _apiHelper.ApiClient.GetAsync("api/Product"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<List<ProductModel>>();
+                    return result;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
