88f70f12c9a402fe7b87e53ee8a09fd604ebb9d2
Rajeev Ravi <thatrajeevkr@gmail.com>
2024-04-20 18:20:42 +0800

    Add SaleDetail.sql

---
+++ TRMData/dbo/Tables/SaleDetail.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[SaleDetail]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [SaleId] INT NOT NULL, 
+    [ProductId] INT NOT NULL, 
+    [PurchasePrice] MONEY NOT NULL, 
+    [Quantity] INT NOT NULL DEFAULT 1,
+    [Tax] MONEY NOT NULL DEFAULT 0, 
+    CONSTRAINT [FK_SaleDetail_ToSale] FOREIGN KEY (SaleId) REFERENCES Sale(Id), 
+    CONSTRAINT [FK_SaleDetail_ToProduct] FOREIGN KEY (ProductId) REFERENCES Product(Id)
+)
