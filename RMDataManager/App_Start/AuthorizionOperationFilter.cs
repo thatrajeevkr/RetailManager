421b63d9cb4fa5da80b2da411f410d1b455bd643
e14rravi <rravi@premierfarnell.com>
2024-04-24 10:32:04 +0100

    Added Swagger

---
+++ RMDataManager/App_Start/AuthorizionOperationFilter.cs
@@ -0,0 +1,30 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+//Add a CS file to add operation filter, ie when every operation runs, add a parameter to 
+//it like bearer token so that the user is authorized in every call
+
+namespace RMDataManager.App_Start
+{
+    public class AuthorizionOperationFilter : IOperationFilter
+    {
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            if(operation.parameters == null)
+            {
+                operation.parameters = new List<Parameter>();
+            }
+            operation.parameters.Add(new Parameter
+            {
+                name = "Authorization",
+                @in = "header",
+                description = "access token",
+                required = false,
+                type = "string"
+            });
+        }
+    }
+}
