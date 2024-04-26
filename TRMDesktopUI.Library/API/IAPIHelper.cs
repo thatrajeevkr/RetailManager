819c2bc82a75409317e9e690e562796187b8d5e2
Rajeev Ravi <thatrajeevkr@gmail.com>
2024-04-26 17:55:39 +0800

    Add IAPIHelper.cs

---
+++ TRMDesktopUI.Library/API/IAPIHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using TRMDesktopUI.Library.Models;
+
+namespace TRMDesktopUI.Library.API
+{
+    public interface IAPIHelper
+    {
+        Task<AuthenticatedUser> Authenticate(string userName, string password);
+        void LogOfUser();
+        Task GetLoggedInUserInfo(string token);
+        HttpClient ApiClient { get; }
+    }
+}
