f297acb545aae67b8a750c8a09197d76ed7055b3
Rajeev Ravi <thatrajeevkr@gmail.com>
2024-04-30 16:37:00 +0800

    Add ProductData.cs

---
+++ TRMDataManager.Library/DataAccess/ProductData.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRMDataManager.Library.Models;
+
+namespace TRMDataManager.Library.DataAccess
+{
+    public class ProductData : IProductData
+    {
+        private readonly ISqlDataAccess _sql;
+
+        public ProductData(ISqlDataAccess sql)
+        {
+            _sql = sql;
+        }
+
+        public List<ProductModel> GetProducts()
+        {
+            var output = _sql.LoadData<ProductModel, dynamic>("dbo.spProduct_GetAll", new { }, "TRMData");
+            return output;
+        }
+
+        public ProductModel GetProductById(int productId)
+        {
+            var output = _sql.LoadData<ProductModel, dynamic>("dbo.spProduct_GetById", new { Id = productId }, "TRMData").FirstOrDefault();
+            return output;
+        }
+    }
+}
