324d617ea73f4e6311aec828c0267369bd8fc2ac
e14rravi <rravi@premierfarnell.com>
2024-04-30 10:34:23 +0100

    Add Sales page
    Viewmodel and XAML

---
+++ RMDesktopUI/ViewModels/SalesViewModel.cs
@@ -0,0 +1,125 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMDesktopUI.ViewModels
+{
+    public class SalesViewModel : Screen
+    {
+        private BindingList<string> products;
+        private string itemQuantity;
+        private BindingList<string> cart;
+
+
+        public BindingList<string> Products
+        {
+            get { return products; }
+            set
+            {
+                products = value;
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
+
+        public BindingList<string> Cart
+        {
+            get { return cart; }
+            set
+            {
+                cart = value;
+                NotifyOfPropertyChange(() => Cart);
+            }
+        }
+
+        public string ItemQuantity
+        {
+            get { return itemQuantity; }
+            set
+            {
+                itemQuantity = value;
+                NotifyOfPropertyChange(() => ItemQuantity);
+            }
+        }
+
+        public string SubTotal
+        {
+            get
+            {
+                // TODO - replace with calculation
+                return "$0.00";
+            }
+        }
+        public string Tax
+        {
+            get
+            {
+                // TODO - replace with calculation
+                return "$0.00";
+            }
+        }
+        public string Total
+        {
+            get
+            {
+                // TODO - replace with calculation
+                return "$0.00";
+            }
+        }
+
+        public bool CanAddToCart
+        {
+            get
+            {
+                bool output = false;
+
+                // Make sure something is selected in the Products listbox
+                // Make sure item quantity is filled
+
+                return output;
+            }
+        }
+
+        public void AddToCart()
+        {
+
+        }
+
+        public bool CanRemoveFromCart
+        {
+            get
+            {
+                bool output = false;
+
+                // Make sure something is selected in the Cart
+
+                return output;
+            }
+        }
+
+        public void RemoveFromCart()
+        {
+
+        }
+
+        public bool CanCheckOut
+        {
+            get
+            {
+                bool output = false;
+
+                // Make sure something is in the Cart
+
+                return output;
+            }
+        }
+
+        public void CheckOut()
+        {
+
+        }
+    }
+}
