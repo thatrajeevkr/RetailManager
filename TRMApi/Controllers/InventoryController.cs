3a6a00e3d7a5f1e45571ab8c420c77d2ba1bb6ba
Rajeev Ravi <thatrajeevkr@gmail.com>
2024-05-08 11:23:15 +0800

    Add InventoryController.cs

---
+++ TRMApi/Controllers/InventoryController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace TRMApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class InventoryController : ControllerBase
+    {
+        private readonly IInventoryData _inventoryData;
+
+        public InventoryController(IInventoryData inventoryData)
+        {
+            _inventoryData = inventoryData;
+        }
+
+        [Authorize(Roles = "Manager,Admin")]
+        [HttpGet]
+        public List<InventoryModel> Get()
+        {
+            return _inventoryData.GetInventory();
+        }
+
+        //[Authorize(Roles ="WarehouseWorker")] If you have to be a warehouseworker and admin to perform this action
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public void Post(InventoryModel item)
+        {
+            _inventoryData.SaveInventoryRecord(item);
+        }
+    }
+}
