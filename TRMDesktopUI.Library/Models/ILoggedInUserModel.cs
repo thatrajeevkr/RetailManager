8b7d5a8ed4ff507088bad269996f0d6d7a589e8f
Rajeev Ravi <thatrajeevkr@gmail.com>
2024-05-09 21:07:47 +0800

    Add ILoggedInUserModel.cs

---
+++ TRMDesktopUI.Library/Models/ILoggedInUserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TRMDesktopUI.Library.Models
+{
+    public interface ILoggedInUserModel
+    {
+        DateTime CreateDate { get; set; }
+        string EmailAddress { get; set; }
+        string FirstName { get; set; }
+        string Id { get; set; }
+        string LastName { get; set; }
+        string Token { get; set; }
+
+        void ResetUserModel();
+    }
+}
