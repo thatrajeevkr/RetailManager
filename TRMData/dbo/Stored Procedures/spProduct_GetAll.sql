ec8f7210016ebaa87672af021680c4a119a8eb7b
Rajeev Ravi <thatrajeevkr@gmail.com>
2024-05-21 12:26:27 +0800

    Add spProduct_GetAll.sql

---
+++ TRMData/dbo/Stored Procedures/spProduct_GetAll.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[spProduct_GetAll]
+AS
+begin
+	set nocount on;
+
+	SELECT Id, ProductName, [Description], RetailPrice, QuantityInStock, IsTaxable, ProductImage
+	from dbo.Product
+	order by ProductName;
+end
