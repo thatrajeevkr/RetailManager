7f7b2ff8a644c31e747283aaa256ceadef95203c
Rajeev Ravi <thatrajeevkr@gmail.com>
2024-05-26 12:31:27 +0800

    Add UserDisplayViewModel.cs

---
+++ TRMDesktopUI/ViewModels/UserDisplayViewModel.cs
@@ -0,0 +1,222 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TRMDesktopUI.Library.API;
+using TRMDesktopUI.Library.Models;
+
+namespace TRMDesktopUI.ViewModels
+{
+    public class UserDisplayViewModel : Screen
+    {
+        private readonly StatusInfoViewModel _status;
+        private readonly IWindowManager _window;
+        private readonly IUserEndpoint _userEndPoint;
+
+        BindingList<UserModel> _users;
+        public BindingList<UserModel> Users
+        {
+            get
+            {
+                return _users;
+            }
+            set
+            {
+                _users = value;
+                NotifyOfPropertyChange(() => Users);
+            }
+        }
+
+        private UserModel _selecteduser;
+
+        public UserModel SelectedUser
+        {
+            get { return _selecteduser; }
+            set 
+            {
+                _selecteduser = value;
+                SelectedUserName = value.Email;
+                UserRoles = new BindingList<string>(value.Roles.Select(x => x.Value).ToList());
+                //TODO - Pull this out into a method
+                LoadRoles().Wait();
+                NotifyOfPropertyChange(() => SelectedUser);
+            }
+        }
+
+        private string _selectedUserRole;
+
+        public string SelectedUserRole
+        {
+            get { return _selectedUserRole; }
+            set
+            {
+                _selectedUserRole = value;
+                NotifyOfPropertyChange(() => SelectedUserRole);
+                NotifyOfPropertyChange(() => CanRemoveSelectedRole);
+            }
+        }
+
+        private string _selectedAvailableRole;
+
+        public string SelectedAvailableRole
+        {
+            get { return _selectedAvailableRole; }
+            set
+            {
+                _selectedAvailableRole = value;
+                NotifyOfPropertyChange(() => SelectedAvailableRole);
+                NotifyOfPropertyChange(() => CanAddSelectedRole);
+            }
+        }
+
+
+        private string _selectedUserName;
+
+        public string SelectedUserName
+        {
+            get 
+            { 
+                return _selectedUserName; 
+            }
+            set 
+            { 
+                _selectedUserName = value; 
+                NotifyOfPropertyChange(() => SelectedUserName);
+            }
+        }
+        private BindingList<string> _UserRoles = new();
+
+        public BindingList<string> UserRoles
+        {
+            get { return _UserRoles; }
+            set 
+            { 
+                _UserRoles = value; 
+                NotifyOfPropertyChange(() => UserRoles);
+            }
+        }
+
+        private BindingList<string> _availableRoles = new();
+
+        public BindingList<string> AvailableRoles
+        {
+            get { return _availableRoles; }
+            set
+            {
+                _UserRoles = value;
+                NotifyOfPropertyChange(() => AvailableRoles);
+            }
+        }
+
+
+
+        public UserDisplayViewModel(StatusInfoViewModel status, IWindowManager window, IUserEndpoint userEndPoint)
+        {
+            _status = status;
+            _window = window;
+            _userEndPoint = userEndPoint;
+        }
+        
+        protected override async void OnViewLoaded(object view)
+        {
+            base.OnViewLoaded(view);
+            try
+            {
+                await LoadUsers();
+            }
+            catch (Exception ex)
+            {
+                dynamic settings = new ExpandoObject();
+                settings.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+                settings.ResizeMode = ResizeMode.NoResize;
+                settings.Title = "System Error";
+
+                //var info = IoC.Get<StatusInfoViewModel>(); -- To get a new instance everytime it is called
+
+                if (ex.Message == "Unauthorized")
+                {
+                    _status.UpdateMessage("Unauthorized Access", "You do not have permission to Interact with the Sales Form");
+                    await _window.ShowDialogAsync(_status, null, settings);
+                }
+                else
+                {
+                    _status.UpdateMessage("Fatal Exception", ex.Message);
+                    await _window.ShowDialogAsync(_status, null, settings);
+                }
+
+                await TryCloseAsync();
+            }
+        }
+
+        private async Task LoadUsers()
+        {
+            var userList = await _userEndPoint.GetAll();
+            Users = new BindingList<UserModel>(userList);
+        }
+
+        private async Task LoadRoles()
+        {
+            var roles = await _userEndPoint.GetAllRoles();
+
+            AvailableRoles.Clear();
+            foreach (var role in roles)
+            {
+                if(UserRoles.IndexOf(role.Value) < 0)
+                {
+                    AvailableRoles.Add(role.Value);
+                }
+            }
+        }
+
+        public bool CanAddSelectedRole
+        {
+            get
+            {
+                if(SelectedUser is null || SelectedAvailableRole is null)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        public bool CanRemoveSelectedRole
+        {
+            get
+            {
+                if (SelectedUser is null || SelectedAvailableRole is null)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        public async void AddSelectedRole()
+        {
+            await _userEndPoint.AdduserToRole(SelectedUser.Id, SelectedAvailableRole);
+
+            UserRoles.Add(SelectedAvailableRole);
+            AvailableRoles.Remove(SelectedAvailableRole);
+        }
+
+        public async void RemoveSelectedRole()
+        {
+            await _userEndPoint.RemoveUserFromRole(SelectedUser.Id, SelectedUserRole);
+
+            AvailableRoles.Add(SelectedUserRole);
+            UserRoles.Remove(SelectedUserRole);
+        }
+    }
+}
