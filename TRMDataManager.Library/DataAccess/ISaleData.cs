71c82c95318f25fdc089bad8deccdae4eb9a97e5
Rajeev Ravi <thatrajeevkr@gmail.com>
2024-05-31 22:14:01 +0800

    Add ISaleData.cs

---
+++ TRMDataManager.Library/DataAccess/ISaleData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using TRMDataManager.Library.Models;
+
+namespace TRMDataManager.Library.DataAccess
+{
+    public interface ISaleData
+    {
+        List<SaleReportModel> GetSaleReport();
+        decimal GetTaxRate();
+        void SaveSale(SaleModel saleInfo, string casherId);
+    }
+}
