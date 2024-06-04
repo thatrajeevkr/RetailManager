8f9a837957f320c0b22a91042a2201490bad6144
Rajeev Ravi <thatrajeevkr@gmail.com>
2024-06-05 01:16:56 +0800

    Add InventoryData.cs

---
+++ TRMDataManager.Library/DataAccess/InventoryData.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRMDataManager.Library.Models;
+
+namespace TRMDataManager.Library.DataAccess
+{
+    public class InventoryData : IInventoryData
+    {
+        private readonly IConfiguration _config;
+        private readonly ISqlDataAccess _sql;
+
+        public InventoryData(IConfiguration config, ISqlDataAccess sql)
+        {
+            _config = config;
+            _sql = sql;
+        }
+
+        public List<InventoryModel> GetInventory()
+        {
+
+            var output = _sql.LoadData<InventoryModel, dynamic>("dbo.spInventory_GetAll", new { }, "TRMData");
+            return output;
+        }
+
+        public void SaveInventoryRecord(InventoryModel item)
+        {
+
+            _sql.SaveData("dbo.spInventory_Insert", item, "TRMData");
+        }
+    }
+}
