d10cf4772339b6f3919789b6dfd8db1c0d7e8743
Rajeev Ravi <thatrajeevkr@gmail.com>
2024-06-26 19:29:58 +0800

    Add CreateUserModel.cs

---
+++ TRMDesktopUI.Library/Models/CreateUserModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRMDesktopUI.Library.Models
+{
+    public class CreateUserModel
+    {
+        [Required]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        [DisplayName("Email Address")]
+        public string EmailAddress { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        [Compare(nameof(Password),ErrorMessage = "The passwords do not match")]
+        [DisplayName("Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
