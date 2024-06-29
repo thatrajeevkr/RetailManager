a50a6862d4c331d6c7f0de1cd5e33b1453e07577
Rajeev Ravi <thatrajeevkr@gmail.com>
2024-06-30 05:57:10 +0800

    Add Bootstrapper.cs

---
+++ TRMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using AutoMapper;
+using Caliburn.Micro;
+using Microsoft.Extensions.Configuration;
+using TRMDesktopUI.Helpers;
+using TRMDesktopUI.Library.API;
+using TRMDesktopUI.Library.Models;
+using TRMDesktopUI.Models;
+using TRMDesktopUI.ViewModels;
+
+namespace TRMDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private SimpleContainer _container = new SimpleContainer();
+        public Bootstrapper()
+        {
+            Initialize();
+
+            ConventionManager.AddElementConvention<PasswordBox>(
+                PasswordBoxHelper.BoundPasswordProperty,
+                "Password",
+                "PasswordChanged");
+        }
+
+        private IMapper ConfigureAutoMapper()
+        {
+            //Configure AutoMapper
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<ProductModel, ProductDisplayModel>();
+                cfg.CreateMap<CartItemModel, CartItemDisplayModel>();
+            });
+
+            var output = config.CreateMapper();
+
+            return output;
+        }
+
+        private IConfiguration AddConfiguration()
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+        #if DEBUG
+                    builder.AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true);
+        #else
+                    builder.AddJsonFile("appsettings.Production.json", optional: true, reloadOnChange: true);
+        #endif
+            return builder.Build();
+        }
+
+        protected override void Configure()
+        {
+
+
+            _container.Instance(ConfigureAutoMapper());
+
+            _container.Instance(_container)
+                .PerRequest<IProductEndpoint, ProductEndpoint>()
+                .PerRequest<IUserEndpoint, UserEndpoint>()
+                .PerRequest<ISaleEndpoint, SaleEndpoint>();
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>()
+                .Singleton<ILoggedInUserModel, LoggedInUserModel>()
+                .Singleton<IAPIHelper, APIHelper>();
+
+            _container.RegisterInstance(typeof(IConfiguration), "IConfiguration", AddConfiguration());
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+    }
+}
